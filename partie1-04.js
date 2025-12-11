7fc1ce7feb9244e686ed745fb4114bb7a99991d4
Jonathan GANIVET <jon.ganivet@gmail.com>
2025-12-11 07:59:39 +0100

    Third exercise

---
+++ partie1-04.js
@@ -0,0 +1,12 @@
+const ticket = {
+    filmName: "Batman",
+    ticketPrice: 10,
+    salleNumber: 5
+}
+const myName = "John Doe";
+console.log(ticket);
+console.log(myName);
+
+let showMessage = "Hello "+ myName +", you have a ticket for the film "+ ticket.filmName +" in salle number "+ ticket.salleNumber;
+console.log(showMessage);
+
