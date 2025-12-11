665da12462f0dbae2f6a91bb26f106fae35dea72
Jonathan GANIVET <jon.ganivet@gmail.com>
2025-12-11 08:15:27 +0100

    Fourth exercise

---
+++ partie1-05.js
@@ -0,0 +1,22 @@
+let playlist = [
+    "Song A",
+    "Song B",
+    "Song C"
+];
+console.log(playlist);
+
+let totalSongs = playlist.length;
+console.log(totalSongs);
+
+playlist.push("Song D");
+playlist.push("Song E");
+console.log(playlist);
+
+totalSongs = playlist.length;
+console.log(totalSongs);
+
+playlist.pop();
+console.log(playlist);
+
+totalSongs = playlist.length;
+console.log(totalSongs);
