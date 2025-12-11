4b9cb1c0f7f6d8cd6126ca4c44688e221df9fade
Jonathan GANIVET <jon.ganivet@gmail.com>
2025-12-11 10:41:41 +0100

    Part 2, second exercise

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-console.log ("Hello World!");
+const wordList = ['apple', 'banana', 'cherry'];
+console.log('Initial word list:', wordList);
+
+let score = 0;
+console.log('Initial score:', score);
+
+let firstWord = prompt('Enter the first word: ' + wordList[0]);
+
+if (firstWord === wordList[0]) {
+    score += 1;
+    console.log('Correct! Score increased to:', score);
+}
+
+let secondWord = prompt('Enter the second word: ' + wordList[1]);
+
+if (secondWord === wordList[1]) {
+    score += 1;
+    console.log('Correct! Score increased to:', score);
+}
+
+let thirdWord = prompt('Enter the third word: '+ wordList[2]);
+
+if (thirdWord === wordList[2]) {
+    score += 1;
+    console.log('Correct! Score increased to:', score);
+}
+
+console.log('Final score:', score);
