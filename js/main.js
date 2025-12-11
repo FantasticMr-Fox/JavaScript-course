656e30a4ece1715f48b1e5058ebc2e720e571d5f
Jonathan GANIVET <jon.ganivet@gmail.com>
2025-12-11 14:14:09 +0100

    Part 2, third exercise

--- js/main.js
+++ js/main.js
@@ -1,28 +1,41 @@
 const wordList = ['apple', 'banana', 'cherry'];
-console.log('Initial word list:', wordList);
+const sentenceList = ['I like to eat', 'My favorite fruit is', 'I enjoy making smoothies with'];
 
 let score = 0;
-console.log('Initial score:', score);
 
-let firstWord = prompt('Enter the first word: ' + wordList[0]);
+let userChoice = prompt('Choose mode: 1 for Words, 2 for Sentences');
 
-if (firstWord === wordList[0]) {
-    score += 1;
-    console.log('Correct! Score increased to:', score);
+if (userChoice === '1') {
+
+    for (let i = 0; i < wordList.length; i++) {
+        let userWord = prompt('Enter word: '+ wordList[i]);
+
+        if (userWord === wordList[i]) {
+            score += 1;
+            console.log('Correct! Score increased to:', score);
+        }
+        else {
+            console.log('Incorrect. The correct word was:', wordList[i]);
+        }
+    }
+
+} else if (userChoice === '2') {
+
+   for (let i = 0; i < sentenceList.length; i++) {
+        let userSentence = prompt('Enter sentence: '+ sentenceList[i]);
+
+        if (userSentence === sentenceList[i]) {
+            score += 1;
+            console.log('Correct! Score increased to:', score);
+        }
+        else {
+            console.log('Incorrect. The correct sentence was:', sentenceList[i]);
+        }
+    } 
+
+} else {
+    console.log('Invalid choice. Please refresh and try again.');
 }
 
-let secondWord = prompt('Enter the second word: ' + wordList[1]);
-
-if (secondWord === wordList[1]) {
-    score += 1;
-    console.log('Correct! Score increased to:', score);
-}
-
-let thirdWord = prompt('Enter the third word: '+ wordList[2]);
-
-if (thirdWord === wordList[2]) {
-    score += 1;
-    console.log('Correct! Score increased to:', score);
-}
 
 console.log('Final score:', score);
