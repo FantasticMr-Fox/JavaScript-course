79293507f28fb426d0685b5317bd48f783b736f2
Jonathan GANIVET <jon.ganivet@gmail.com>
2025-12-16 14:48:15 +0100

    Part 3 - first exercices

--- js/main.js
+++ js/main.js
@@ -4,4 +4,19 @@
 *
 *********************************************************************************/
 
-startGame();
+//startGame();
+
+let userInput = document.getElementById('userInput');
+console.log(userInput);
+
+let validateButton = document.getElementById('validateButton');
+console.log(validateButton);
+
+let suggestionZone = document.querySelector('.suggestionZone');
+console.log(suggestionZone);
+
+let scoreZone = document.querySelector('.scoreZone span');
+console.log(scoreZone);
+
+let userChoice = document.querySelectorAll('.userChoice input');
+console.log(userChoice);
